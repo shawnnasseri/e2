ee2dc41b8df9163e7461847c7b2c18a02747e6e2
Shawn Nasseri <captain@scuba-now.com>
2022-09-15 02:05:34 +0000

    piggy update

--- Practice/bank.php
+++ Practice/bank.php
@@ -33,7 +33,7 @@ $total = ($pennies * $penny_value) + ($nickels * $nickel_value) + ($dimes * $dim
     <img alt='PHPiggy Bank Logo' src='https://s3.amazonaws.com/making-the-internet/php-piggy-bank-logo@2x.png' style='width:202px;'>
 
     <p>
-        You have $<?php echo $total; ?> in your piggy bank.
+        You have. $<?php echo $total; ?> in your piggy bank.
     </p>
 
 </body>
