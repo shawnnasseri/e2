f79b511f1226f55e199d02f36f0c8d256e9fed1c
Shawn Nasseri <captain@scuba-now.com>
2022-10-03 15:59:04 +0000

    Quick Test Game

--- p1/index.php
+++ p1/index.php
@@ -1,3 +1,36 @@
-<h1>Shawn Nasseri Test In P1 Latest </h1>
-<!-- $view = View::make('index-view')->with('name', 'Shawn Nasseri PHP'); -->
- 
+<h1>Shawn Nasseri In E2 P1 </h1>
+<h1><?php echo "Start of Project" ?></h1>
+
+<?php
+
+$number= $_POST['number_in'];
+
+$submitbutton= $_POST['submit'];
+
+$randomnumber= mt_rand(1,20);
+
+?>
+<form action="" method="POST">
+Guess A number between 1 and 20: 
+<input type="text" name="number_in" value=''/> <br><br>
+
+Number In: 
+<?php 
+if ($submitbutton){
+
+if (($number > 0) && ($number <=20)){
+if ($number != $randomnumber)
+{
+echo "Incorrect. Number was $randomnumber. Try again";
+}
+else 
+{
+echo "$randomnumber correct.";
+}
+}
+
+}
+?>
+<br><br>
+<input type="submit" name="submit" value="Search"/><br><br>
+</form>
