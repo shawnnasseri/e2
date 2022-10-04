ed7eaa827d11c7ee382fc9fd1b443f118fe55d20
Shawn Nasseri <captain@scuba-now.com>
2022-10-04 20:27:44 +0000

    Color fix

--- p1/index.php
+++ p1/index.php
@@ -1,18 +1,28 @@
-<h1>Shawn Nasseri In E2 P1 </h1>
-<h1><?php echo "Start of Project" ?></h1>
+
+<!-- Author:   Shawn Nasseri
+     Date:     October 4, 2022
+     Function: Simple game of guessing numbers
+             : between 1 and 20 Will say if guess is high or low
+             : Two random number functions
+-->
+
+<h1>Shawn Nasseri E2 P1 </h1>
+<h1> <?php echo "Start of Project One"?> </h1>
 
 <?php
 $rand = rand(1,20);
 for ($x = 0; $x <= 100; $x++) {
    echo " Count is: $x <br> ";
    $guess_rand = rand(1,20);
-   echo " Random Guess is: $guess_rand <br> ";
+   echo "<span style='color:magenta;'>Target number is: $rand </span<br> ";
+   echo "</br>";
+   echo "<span style='color:brown;'> Random Guess is: $guess_rand </span><br> ";
    if ($guess_rand > $rand) {
-       echo " guess high ";
+       echo "<span style='color:red;'> guess high </span>";
    } elseif ($guess_rand == $rand) {
-       echo " guess correct ";
+       echo "<span style='color:black,Strong;'> ------------correct guess--------- </span>";
    } else {
-       echo " Guess low ";
+       echo "<span style='color:green;'> guess low </span>";
 }
 }
 ?>
