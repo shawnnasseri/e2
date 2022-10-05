8845258fa8ede8d96b1ec83a6d84b14a104f362e
Shawn Nasseri <captain@scuba-now.com>
2022-10-05 03:11:14 +0000

    mode view fix

---
+++ p1/index-view.php
@@ -0,0 +1,22 @@
+
+<!-- Author:   Shawn Nasseri
+     Date:     October 4, 2022
+     Function: Simple game of guessing numbers
+             : between 1 and 20 Will say if guess is high or low
+             : Two random number functions view file
+-->
+<head>
+  <link rel="stylesheet" href="styles.css">
+</head>
+
+<h1>Shawn Nasseri E2 P1 </h1>
+<h1>Start of Project One</h1>
+
+<h2>Counter Trys to guess the number</h2>
+<h2><?=$x?></h2>
+<h2>New Random Guess Generated
+<p><?=$guess_rand?></p>
+<h2>Original Random Number Generated
+<p><?=$rand?></p>
+<h2>Guess is <?=$guess_code?> Original No <?=$rand?> </h2>
+<h3>Last Guess that matched was <?=$guess_rand?></h3>
--- p1/index.php
+++ p1/index.php
@@ -3,26 +3,21 @@
      Date:     October 4, 2022
      Function: Simple game of guessing numbers
              : between 1 and 20 Will say if guess is high or low
-             : Two random number functions
+             : Two random number functions. This is the controller
 -->
-
-<h1>Shawn Nasseri E2 P1 </h1>
-<h1> <?php echo "Start of Project One"?> </h1>
 
 <?php
 $rand = rand(1,20);
 for ($x = 0; $x <= 100; $x++) {
-   echo " Count is: $x <br> ";
    $guess_rand = rand(1,20);
-   echo "<span style='color:magenta;'>Target number is: $rand </span<br> ";
-   echo "</br>";
-   echo "<span style='color:brown;'> Random Guess is: $guess_rand </span><br> ";
    if ($guess_rand > $rand) {
-       echo "<span style='color:red;'> guess high </span>";
-   } elseif ($guess_rand == $rand) {
-       echo "<span style='color:black,Strong;'> ------------correct guess--------- </span>";
+       $guess_code = 'guess high';
+   } elseif ($guess_rand < $rand) {
+       $guess_code = 'guess low';
    } else {
-       echo "<span style='color:green;'> guess low </span>";
+       $guess_code = 'correct guess';
+       break;
 }
 }
+require "index-view.php"
 ?>
