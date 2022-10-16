59ab693e9a35f3e9e33b01d3cc9ae2b268511093
Shawn Nasseri <captain@scuba-now.com>
2022-10-16 02:41:32 +0000

    more refactor changes

---
+++ p2/README.md
@@ -0,0 +1,15 @@
+Any instructions/notes in italics should be removed from the template before submitting
+
+Project 1
+By: Shawn S Nasseri
+URL: http://e2p1.computersoft.click
+Game planning
+
+Guess the number
+
+Create a loop from 0 to 100 to guess random number from 1 to 20. 
+During the loop another random value is picked and compared to
+see if it is too low or high or matches the first random value 1 to 20.
+
+
+Notes for instructor
---
+++ p2/index-view.php
@@ -0,0 +1,31 @@
+
+<!-- Author:   Shawn Nasseri
+     Date:     October 15, 2022
+     Function: Simple game of guessing numbers
+             : between 1 and 20 Will say if guess is high or low
+             : Two random number functions view file
+             : Refactor ***
+-->
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Project 1</title>
+    <meta charset='utf-8'>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>  
+    <h1>Shawn Nasseri E2 P1 </h1>
+    <h1>Start of Project One Guess number 1 to 20</h1>
+
+    <h2>Counter Trys to guess the number</h2>
+    <h2><?=$x?></h2>
+    <h2>New Random Guess Generated
+    <p><?=$guess_rand?></p>
+    <h2>Original Random Number Generated
+    <p><?=$rand?></p>
+    <h2>Guess is <?=$guess_code?> Original No <?=$rand?> </h2>
+    <h3>Last Guess that matched was <?=$guess_rand?></h3>
+</body>
+</html>
